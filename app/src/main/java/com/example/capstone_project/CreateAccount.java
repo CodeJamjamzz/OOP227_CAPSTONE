f0effff0e98c2f57d039986bee34013317605273
Jamiel Kyne Pinca <jamiel062705@gmail.com>
2024-11-25 08:26:38 +0800

    added create account activity

--- app/src/main/java/com/example/capstone_project/CreateAccount.java
+++ app/src/main/java/com/example/capstone_project/CreateAccount.java
@@ -2,7 +2,8 @@ package com.example.capstone_project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashScreen extends AppCompatActivity {
+public class CreateAccount extends AppCompatActivity {
 
+    TextView QRCodecreate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_create_account);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.studentnumber), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 2000);
+        QRCodecreate = (TextView) findViewById(R.id.QRCodeCreateActivity);
+    }
 
+    public void createQRCodeActivity(View view){
+        startActivity(new Intent(CreateAccount.this, CreateQRCode.class));
     }
 }
--- app/src/main/java/com/example/capstone_project/MainActivity.java
+++ app/src/main/java/com/example/capstone_project/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.capstone_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView createAccount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.studentnumber), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        createAccount = (TextView) findViewById(R.id.createAccount);
+    }
+
+    public void createAccountActivity(View view){
+        startActivity(new Intent(MainActivity.this, CreateAccount.class));
     }
 }
