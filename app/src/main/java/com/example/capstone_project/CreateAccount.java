845087a0c509f6aaec008cc43f998e4b52947560
Jamiel Kyne Pinca <jamiel062705@gmail.com>
2024-11-28 02:00:43 +0800

    updated layout ui/ux

--- app/src/main/java/com/example/capstone_project/CreateAccount.java
+++ app/src/main/java/com/example/capstone_project/CreateAccount.java
@@ -3,6 +3,7 @@ package com.example.capstone_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,11 @@ import androidx.core.view.WindowInsetsCompat;
 public class CreateAccount extends AppCompatActivity {
     // open activity_create_account.xml
     TextView QRCodecreate;
+    EditText InputedName;
+    EditText InputedStudentNumber;
+    EditText InputedEmail;
+    EditText InputedCourseYear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +35,28 @@ public class CreateAccount extends AppCompatActivity {
         QRCodecreate = findViewById(R.id.QRCodeCreateActivity);
     }
 
+    public void getInformationCreateAccount(Intent nextActivity){
+        InputedName = findViewById(R.id.inputName);
+        InputedStudentNumber = findViewById(R.id.inputStudentNumber);
+        InputedEmail = findViewById(R.id.inputEmail);
+        InputedCourseYear = findViewById(R.id.inputCourseYear);
+
+        String Name = InputedName.getText().toString();
+        String StudentNumber = InputedStudentNumber.getText().toString();
+        String Email = InputedEmail.getText().toString();
+        String CourseYear = InputedCourseYear.getText().toString();
+
+        nextActivity.putExtra("InputedName", Name);
+        nextActivity.putExtra("InputedStudentNumber", StudentNumber);
+        nextActivity.putExtra("InputedEmail", Email);
+        nextActivity.putExtra("InputedCourseYear", CourseYear);
+        return;
+    }
+
     // create account button; need to add qr generation and input verification
     public void createQRCodeActivity(View view){
-        startActivity(new Intent(CreateAccount.this, CreateQRCode.class));
+        Intent CreateQrCode = new Intent(CreateAccount.this, CreateQRCode.class);
+        getInformationCreateAccount(CreateQrCode);
+        startActivity(CreateQrCode);
     }
 }
--- app/src/main/java/com/example/capstone_project/CreateQRCode.java
+++ app/src/main/java/com/example/capstone_project/CreateQRCode.java
@@ -1,6 +1,10 @@
 package com.example.capstone_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +15,44 @@ import androidx.core.view.WindowInsetsCompat;
 // java code for activity_create_qrcode.xml screen
 public class CreateQRCode extends AppCompatActivity {
     // open activity_create_qrcode.xml
+
+    //different textView for the display
+    TextView DisplayName;
+    TextView DisplayStudentNumber;
+    TextView DisplayEmail;
+    TextView DisplayCourseYear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_qrcode);
+
+        DisplayName = findViewById(R.id.outputNameDisplay);
+        DisplayStudentNumber = findViewById(R.id.outputStudentNumberDisplay);
+        DisplayEmail = findViewById(R.id.outputEmailDisplay);
+        DisplayCourseYear = findViewById(R.id.outputCourseYearDisplay);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        DisplayInfo();
+    }
+
+    public void DisplayInfo(){
+        Intent previousActivity = getIntent();
+        DisplayName.setText(previousActivity.getStringExtra("InputedName"));
+        DisplayStudentNumber.setText(previousActivity.getStringExtra("InputedStudentNumber"));
+        DisplayEmail.setText(previousActivity.getStringExtra("InputedEmail"));
+        DisplayCourseYear.setText(previousActivity.getStringExtra("InputedCourseYear"));
+    }
+
+    // function to return to main page
+    public void returnMainActivity(View view){
+        startActivity(new Intent(CreateQRCode.this , MainActivity.class));
     }
 
     // need to add qr file handling and display, downloading qr, and other stuff
