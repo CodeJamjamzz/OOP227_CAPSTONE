57154a9fc22fbd848357bbaf7d872ee0ceae0f64
jewican <james.ewican@cit.edu>
2024-11-30 00:25:24 +0800

    made initial layout for admin dashboard

---
+++ app/src/main/java/com/example/capstone_project/AdminDashboard.java
@@ -0,0 +1,13 @@
+package com.example.capstone_project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminDashboard extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+    }
+}
