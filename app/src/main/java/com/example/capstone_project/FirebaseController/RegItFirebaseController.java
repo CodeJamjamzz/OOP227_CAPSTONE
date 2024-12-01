34722a81a6fd666f694912c8defe34cbdb77ce2d
SuperaNova <jared.acebes@gmail.com>
2024-12-02 04:12:40 +0800

    added basic account creation

--- app/src/main/java/com/example/capstone_project/FirebaseController/RegItFirebaseController.java
+++ app/src/main/java/com/example/capstone_project/FirebaseController/RegItFirebaseController.java
@@ -2,7 +2,7 @@ package com.example.capstone_project.FirebaseController;
 
 
 //import android.util.Log;
-//
+
 //import androidx.annotation.NonNull;
 
 import com.example.capstone_project.models.UserAccount;
